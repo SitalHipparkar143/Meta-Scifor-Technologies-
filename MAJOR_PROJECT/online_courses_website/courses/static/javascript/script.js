716f6860c95b69c1aba45b1e819b44e055cc4f62
SitalHipparkar143 <144315636+SitalHipparkar143@users.noreply.github.com>
2025-03-23 22:41:54 +0530

    Delete MAJOR_PROJECT/online_courses_website directory

--- MAJOR_PROJECT/online_courses_website/courses/static/javascript/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const searchButton = document.getElementById("search-btn");
-    searchButton.addEventListener("click", function() {
-        const searchQuery = document.getElementById("search-input").value;
-        alert("You searched for: " + searchQuery);
-    });
-});
